6c008039bb6dba9c742ef29b2d2bc08f0bb3123b
antoine chabreuil <achabreuil@openstudio.fr>
2021-11-24 17:00:03 +0100

    listener ajout colonne ok

--- EventListener/DataTableAddColumnListener.php
+++ EventListener/DataTableAddColumnListener.php
@@ -5,6 +5,11 @@ namespace ProductStatus\EventListener;
 use EasyProductManager\Events\DataTableAddColumn;
 use EasyProductManager\Events\DataTableColumnData;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Thelia\Core\HttpFoundation\Request;
+use Thelia\Model\Product;
+use Thelia\Model\ProductQuery;
 
 class DataTableAddColumnListener implements EventSubscriberInterface
 {
@@ -18,10 +23,45 @@ class DataTableAddColumnListener implements EventSubscriberInterface
     }
 
     public function onColumnAdded(DataTableAddColumn $event){
-        $data = $event->getData();
+//        $event->addColumn(['title'=>'Statut','orderable'=>true]);
     }
 
     public function onColumnDataAdded(DataTableColumnData $event){
+        /** @var ProductQuery $query */
+        $query = $event->getQuery();
+        $request = $event->getRequest();
 
+        foreach ($event->getNewColumns() as $newColumn){
+            $query->withColumn($newColumn[0],$newColumn[1]);
+
+        }
+
+        // colonne statut
+        $products = $query->limit($this->getLength($request))->find();
+
+        /** @var Product $product */
+        foreach ($products as $product) {
+//            // Jointure product statut
+//            $query->useProductProductStatusQuery()
+//                ->filterByProductId($product->getId())
+//                ->useProductStatusQuery('productStatus')
+//                ->endUse()
+//                ->endUse()
+//            ;
+//            $statut = $product->getVirtualColumn('statut');
+        }
+
+        $event->setQuery($query);
     }
+
+    /**
+     * @param Request $request
+     * @return int
+     */
+    protected function getLength(Request $request)
+    {
+        return (int) $request->get('length');
+    }
+
+
 }
